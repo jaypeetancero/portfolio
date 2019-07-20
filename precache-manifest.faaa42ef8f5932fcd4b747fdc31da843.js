b352ead455bbdbddddff76624567de809b4cf156
jaypeetan <jaypee.tan@codegum.ph>
2019-07-20 12:43:42 +0800

    Updates

---
+++ precache-manifest.faaa42ef8f5932fcd4b747fdc31da843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d8edba13b89308842b2f774d68b2fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3928fe6ffb913a6df0c9",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/portfolio/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "3928fe6ffb913a6df0c9",
+    "url": "/portfolio/static/js/main.c754db79.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
